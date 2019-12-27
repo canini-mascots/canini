7e5e99e17e95d362ed759d074d114fd6f6fee0a7
Juan Ferrer Toribio <juan@verdnatura.es>
2019-12-27 19:06:55 +0100

    Deploy fixes

--- back/server/server.js
+++ back/server/server.js
@@ -20,6 +20,7 @@ app.start = function() {
     if (app.get('loopback-component-explorer')) {
       var explorerPath = app.get('loopback-component-explorer').mountPath;
       console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
+      console.log('Started!');
     }
   });
 };
