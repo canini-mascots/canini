439da4d06c92eb695bd2c8726d0bfda4a10c7224
xusimr97 <47919041+xusimr97@users.noreply.github.com>
2020-04-25 11:52:55 +0200

    #16 Index page (#20)
    * Error 404 page
    
    * #16 Index page
    
    * #16 fixed Index Page
    
    Co-authored-by: Juan Ferrer Toribio <juan@verdnatura.es>
    Co-authored-by: Josep Miquel Manzanera Roma <josep@lengova.com>

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,78 +1,87 @@
-
-const routes = [
-  {
+const routes = [{
     path: '/',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [
-      {
+    component: () =>
+        import ('layouts/MainLayout.vue'),
+    children: [{
         name: '',
         path: '',
-        component: () => import('pages/Index.vue')
-      }, {
+        component: () =>
+            import ('pages/Index.vue')
+    }, {
         name: 'home',
         path: 'home',
-        component: () => import('pages/Index.vue')
-      }, {
+        component: () =>
+            import ('pages/Index.vue')
+    }, {
         name: 'catalog',
         path: 'catalog/:type?',
-        component: () => import('pages/Catalog.vue')
-      }, {
+        component: () =>
+            import ('pages/Catalog.vue')
+    }, {
         name: 'orders',
         path: 'orders',
-        component: () => import('pages/Orders.vue')
-      }, {
+        component: () =>
+            import ('pages/Orders.vue')
+    }, {
         name: 'conditions',
         path: 'conditions',
-        component: () => import('pages/Conditions.vue')
-      }, {
+        component: () =>
+            import ('pages/Conditions.vue')
+    }, {
         name: 'about',
         path: 'about',
-        component: () => import('pages/About.vue')
-      }, {
+        component: () =>
+            import ('pages/About.vue')
+    }, {
         name: 'account',
         path: 'account',
-        component: () => import('pages/Account.vue')
-      }, {
+        component: () =>
+            import ('pages/Account.vue')
+    }, {
         name: 'suscriptions',
         path: 'suscriptions',
-        component: () => import('pages/Suscriptions.vue')
-      }, {
+        component: () =>
+            import ('pages/Suscriptions.vue')
+    }, {
         name: 'myMascots',
         path: 'my-mascots',
-        component: () => import('pages/MyMascots.vue')
-      }
-    ]
-  }, {
+        component: () =>
+            import ('pages/MyMascots.vue')
+    }]
+}, {
     path: '/login',
-    component: () => import('layouts/LoginLayout.vue'),
-    children: [
-      {
+    component: () =>
+        import ('layouts/LoginLayout.vue'),
+    children: [{
         name: 'login',
         path: '',
-        component: () => import('pages/Login.vue')
-      }, {
+        component: () =>
+            import ('pages/Login.vue')
+    }, {
         name: 'rememberPassword',
         path: 'remember-password',
-        component: () => import('pages/RememberPassword.vue')
-      }, {
+        component: () =>
+            import ('pages/RememberPassword.vue')
+    }, {
         name: 'setPassword',
         path: '/set-password',
-        component: () => import('pages/SetPassword.vue')
-      }, {
+        component: () =>
+            import ('pages/SetPassword.vue')
+    }, {
         name: 'register',
         path: 'register',
-        component: () => import('pages/Register.vue')
-      }
-    ]
-  }
-]
+        component: () =>
+            import ('pages/Register.vue')
+    }]
+}]
 
 // Always leave this as last one
 if (process.env.MODE !== 'ssr') {
-  routes.push({
-    path: '*',
-    component: () => import('pages/Error404.vue')
-  })
+    routes.push({
+        path: '*',
+        component: () =>
+            import ('pages/Error404.vue')
+    })
 }
 
 export default routes
