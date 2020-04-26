bbfb787fd3907f0b01a319728c74a8eab4165d28
Juan Ferrer Toribio <juan@verdnatura.es>
2020-04-26 14:06:46 +0200

    #21 Deploy fixes

--- back/gcloud.js
+++ back/gcloud.js
@@ -1,41 +1,41 @@
 
-const env = process.env.NODE_ENV
+const env = process.env.NODE_ENV;
 if (!env || env === 'development') {
-  console.log(`Running in development environment, leaving.`)
-  process.exit()
+  console.log('Running in development environment, leaving.');
+  process.exit();
 }
 
-const fs = require('fs')
-const backDir = 'server'
+const fs = require('fs');
+const confDir = 'server';
 const confFiles = [
   'datasources.local.json',
-  'config.local.json'
-]
+  'config.local.json',
+];
 
-const downloadFiles = []
+const downloadFiles = [];
 for (let file of confFiles) {
-  if (!fs.existsSync(`${backDir}/${file}`)) {
-    downloadFiles.push(file)
+  if (!fs.existsSync(`${confDir}/${file}`)) {
+    downloadFiles.push(file);
   }
 }
 if (!downloadFiles.length) {
-  console.log(`Config files already exist.`)
-  process.exit()
+  console.log('Config files already exist.');
+  process.exit();
 }
 
-const gcs = require('@google-cloud/storage')
-const bucketName = 'datasources.canini.es'
-const storage = new gcs.Storage()
+const gcs = require('@google-cloud/storage');
+const bucketName = 'datasources.canini.es';
+const storage = new gcs.Storage();
 
-const tasks = []
+const tasks = [];
 for (let file of confFiles) {
-  console.log(`Downloading '${file}' from bucket '${bucketName}.'`)
+  console.log(`Downloading '${file}' from bucket '${bucketName}.'`);
   tasks.push(storage
     .bucket(bucketName)
     .file(file)
-    .download({ destination: `${backDir}/${file}` }))
+    .download({destination: `${confDir}/${file}`}));
 }
 
 Promise.all(tasks)
-  .then(() => console.info('Config files downloaded successfully.'))
-  .catch(err => console.error(`Error: ${err}.`))
+  .then(() => console.info('Config files downloaded successfully.'));
+// .catch(err => console.error(`Error: ${err}.`))
